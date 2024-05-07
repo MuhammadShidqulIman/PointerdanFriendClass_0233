22c69620649c4920fffddfdaecbf364c1ae1faee
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-07 14:57:16 +0700

    Membuat akses persegi panjang

--- Pointer dan Friend clas/Pointer dan Friend clas.cpp
+++ Pointer dan Friend clas/Pointer dan Friend clas.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class PersegiPanjang
+{
+
+public:
+    int panjang, lebar;
+    int luasPersegiPanjang(int b, int a)
+    {
+        return a * b;
+    }
+};
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
